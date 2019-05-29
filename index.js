443f4fd7322d2ee5d15f050e00d64c6ba0f184b6
Pedro Chinen <chinen93@gmail.com>
2019-05-29 10:00:22 -0300

    Add phone handler and comments to code

--- index.js
+++ index.js
@@ -5,19 +5,31 @@ const _ = require('lodash');
 const fs = require('fs');
 const readline = require('readline');
 
+// Get an instance of `PhoneNumberUtil`.
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
 
 // Code
 let lines = [];
-let objects = [];
+let people = [];
 
 
-// Function to Get a String and convert it into a array
+// ===================================================================
+// Function to Get a String and convert it into a array.
+// Parameters: line => String, representing a line from a CSV file.
 function splitLine(line){
   console.log(line);
+
+  // Line is a line from a csv file, so split everything based on comma.
   let middle = _.split(line, ',');
   let res = [];
+
   for(let i=0; i<middle.length; i++){
+
     if(_.startsWith(middle[i], '"')){
+      // The base csv data has some dirty characters in places that need
+      // to be fixed.  For example: "email Responsável, Pai".
+      // Where exists a comma separator in the middle of the value.
       let string = '';
       string += _.replace(middle[i], '"', '');
       string += ',';
@@ -25,16 +37,22 @@ function splitLine(line){
       i++;
       res.push(string);
     }else{
+
+      // There is no need to clean, so just add it to the return.
       res.push(middle[i]);
     }
   }
   return res;
 }
 
-// Function to Get a Array and create an Object with each element as a key
-function transforIntoObject(keys, line){
-  let object = {};
+// ===================================================================
+// Function to transforma a line into a person object
+// Parameters: keys => Object keys.
+//             line => Values for each key.
+function transforIntoPerson(keys, line){
+  let person = {};
 
+  // For each pair key-value
   for(let i=0; i<keys.length; i++){
     key = keys[i];
     value = line[i];
@@ -43,15 +61,20 @@ function transforIntoObject(keys, line){
     // Handle Classes
     if(_.startsWith(value, 'Sala')){
 
-      if(!Array.isArray(object['classes'])){
-        object['classes'] = [];
+      // Classes location for this person
+      if(!Array.isArray(person['classes'])){
+        person['classes'] = [];
       }
 
+      // Some classes may have a dirty character in the middle.
+      // For example: "Sala 1 / Sala 2"
       if(value.indexOf('/')){
         let values = _.split(value, '/');
         _.forEach(values, function(val){
-          object['classes'].push(_.trimEnd(val));
+          person['classes'].push(_.trimEnd(val));
         });
+      }else{
+        person['classes'].push(value);
       }
 
     }else{
@@ -60,10 +83,16 @@ function transforIntoObject(keys, line){
       if(_.startsWith(key, 'email') ||
          _.startsWith(key, 'phone')){
 
-        if(!Array.isArray(object['addresses'])){
-          object['addresses'] = [];
+        // Control variable to add or not an address into the
+        // addresses key.
+        let addAddress = true;
+
+        if(!Array.isArray(person['addresses'])){
+          person['addresses'] = [];
         }
 
+        // Some keys may have a dirty character in the middle.
+        // For example: "email Responsável, Pai"
         key = _.replace(key, ',', '');
         keyElem = _.split(key, ' ');
         address = {};
@@ -71,24 +100,31 @@ function transforIntoObject(keys, line){
         address['tags'] = keyElem;
 
         if(_.startsWith(key, 'phone')){
-          address['address'] = value;
+          try{
+            // Parse number with country code and keep raw input.
+            const number = phoneUtil.parseAndKeepRawInput(value, 'BR');
+            address['address'] = value;
+          }catch(err){
+            // Could not parse the number, so do not add it to
+            // addresses.
+            addAddress = false;
+          }
         }else{
+          // Address is an email, just add it.
           address['address'] = value;
         }
 
-        object['addresses'].push(address);
+        if(addAddress){
+          person['addresses'].push(address);
+        }
       }else{
-        object[key] = value;
+        person[key] = value;
       }
     }
   }
-  console.log(JSON.stringify(object));
-  return object;
+  console.log(JSON.stringify(person));
+  return person;
 }
-// let aux = "bla";
-// object[aux] = [];
-// object[aux].push("oi");
-// object[aux].push("oi");
 
 // console.log(JSON.stringify(object));
 
@@ -110,9 +146,10 @@ async function processLineByLine() {
   }
 
   let keysLine = lines.shift();
-  console.log(lines[1]);
-  let object = transforIntoObject(keysLine, lines[0]);
-  objects.push(object);
+  _.forEach(lines, function(line){
+    let person = transforIntoPerson(keysLine, line);
+    people.push(person);
+  });
   // console.log(lines);
 }
 processLineByLine();
